f1f36b114cc78993b5eafcafd3a2b81b2ddff70a
andi.rayka <andi.rayka@gmail.com>
2022-08-03 07:58:03 +0700

    fix homepage link

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://github.com/andirayka/portfolio-andirayka',
+  homepage: 'https://andirayka.netlify.app/',
   title: 'DEV',
 }
 
