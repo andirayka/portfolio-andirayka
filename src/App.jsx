c95bc0002749a7750f404bccbbdc1e8091250c53
andi.rayka <andi.rayka@gmail.com>
2023-02-02 11:50:47 +0700

    test: remove css from about.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import About from './components/about/About'
-import Contact from './components/contact/Contact'
-import Experience from './components/experience/Experience'
-import Footer from './components/footer/Footer'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import Portfolio from './components/portfolio/Portfolio'
-import Services from './components/services/Services'
+// import Contact from './components/contact/Contact'
+// import Experience from './components/experience/Experience'
+// import Footer from './components/footer/Footer'
+// import Header from './components/header/Header'
+// import Nav from './components/nav/Nav'
+// import Portfolio from './components/portfolio/Portfolio'
+// import Services from './components/services/Services'
 
 function App() {
   return (
     <>
-      <Header />
-      <Nav />
+      {/* <Header />
+      <Nav /> */}
       <About />
-      <Experience />
+      {/* <Experience />
       <Services />
       <Portfolio />
       <Contact />
-      <Footer />
+      <Footer /> */}
     </>
   )
 }
